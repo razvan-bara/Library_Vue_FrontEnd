2a08b5a21b5dd9c769aeb6843a14e3444a75e0e3
razvan-bara <rzvbara@gmail.com>
2022-10-05 00:00:25 +0300

    added font-awsome solid icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import './assets/styles/style.css';
 import App from './App.vue';
 import router from './router';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .mount('#app');
