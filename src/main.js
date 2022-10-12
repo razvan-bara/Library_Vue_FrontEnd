9ffe52647fc954f5a75d5228c4bc195c4a8beb9d
razvan-bara <rzvbara@gmail.com>
2022-10-12 17:33:27 +0300

    start using pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import './assets/styles/reset.css';
 import './assets/styles/style.css';
 import App from './App.vue';
+
+import { createPinia } from 'pinia'
 import router from './router';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -9,8 +11,10 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(fas)
+const pinia = createPinia()
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
+    .use(pinia)
     .use(router)
     .mount('#app');
